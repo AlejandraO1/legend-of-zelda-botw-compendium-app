6a20834becd5c2df57e3a84ed20985ac086e1af8
AlejandraO1 <alejandraortegax@gmail.com>
2023-04-06 11:30:22 -0400

    Showing an entry's data in conjuncture with the API and deleting placeholders

--- app.js
+++ app.js
@@ -1,5 +1,21 @@
 function displayItemEntry(response) {
-  console.log(response.data);
+  let nameElement = document.querySelector("#name");
+  nameElement.innerHTML = response.data.data.name;
+  let numberElement = document.querySelector("#number");
+  numberElement.innerHTML = response.data.data.id;
+  let categoryElement = document.querySelector("#category");
+  categoryElement.innerHTML = response.data.data.category;
+  let descriptionElement = document.querySelector("#description");
+  descriptionElement.innerHTML = response.data.data.description;
+  let locationElement = document.querySelector("#location");
+  locationElement.innerHTML = response.data.data.common_locations;
+  let dropsElement = document.querySelector("#drops");
+  dropsElement.innerHTML = response.data.data.drops;
+  let itemImageElement = document.querySelector("#item-image");
+  itemImageElement.setAttribute(
+    "src",
+    `https://botw-compendium.herokuapp.com/api/v2/entry/${response.data.data.name}/image`
+  );
 }
 
 let entry = "Lord of the Mountain";
