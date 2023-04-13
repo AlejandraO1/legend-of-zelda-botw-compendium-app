289e0f88109476efb0afac2635fb7145091200ef
AlejandraO1 <alejandraortegax@gmail.com>
2023-04-13 16:43:07 -0400

    Added all entries to creatures page in list form

--- javascript/creatures.js
+++ javascript/creatures.js
@@ -1,22 +1,36 @@
 function showAllCreatures(response) {
-  let creatures = response.data.data.food;
+  const creatures = response.data.data.food;
+  let container = document.querySelector("#creature-container");
+
   creatures.forEach((creature) => {
-    let nameElement = document.querySelector(".creature-name");
-    nameElement.innerHTML = creature.name;
-    let numberElement = document.querySelector(".creature-number");
-    numberElement.innerHTML = creature.id;
-    let descriptionElement = document.querySelector(".creature-description");
-    descriptionElement.innerHTML = creature.description;
-    let locationElement = document.querySelector(".creature-location");
-    locationElement.innerHTML = creature.common_locations;
-    let dropsElement = document.querySelector(".creature-drops");
-    dropsElement.innerHTML = creature.drops;
-    let creatureImageElement = document.querySelector(".creature-image");
-    creatureImageElement.setAttribute(
-      "src",
-      `https://botw-compendium.herokuapp.com/api/v2/entry/${creature.name}/image`
-    );
-    console.log(creature);
+    let creatureList = document.createElement("ul");
+    let nameItem = document.createElement("li");
+    nameItem.innerHTML = `<strong>Name: </strong>${creature.name}`;
+
+    creatureList.appendChild(nameItem);
+    let numberItem = document.createElement("li");
+    numberItem.innerHTML = `<strong>ID: </strong>${creature.id}`;
+    creatureList.appendChild(numberItem);
+
+    let descriptionItem = document.createElement("li");
+    descriptionItem.innerHTML = ` <strong>Description: </strong>${creature.description}`;
+    creatureList.appendChild(descriptionItem);
+
+    let locationItem = document.createElement("li");
+    locationItem.innerHTML = `<strong>Location: </strong>${creature.common_locations}`;
+    creatureList.appendChild(locationItem);
+
+    let dropsItem = document.createElement("li");
+    dropsItem.innerHTML = `<strong>Drops: </strong>${creature.drops}`;
+    creatureList.appendChild(dropsItem);
+
+    let imageItem = document.createElement("li");
+    let image = document.createElement("img");
+    image.src = creature.image;
+    imageItem.appendChild(image);
+    creatureList.appendChild(imageItem);
+
+    container.appendChild(creatureList);
   });
 }
 
